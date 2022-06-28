9c475b14ba1dba10565aba613472ccf62c5063e9
Saulo-Felipe <saulofelipe234567@gmail.com>
2022-06-27 21:44:34 -0300

    fixing list 8 errors

--- List08/Ex01.java
+++ List08/Ex01.java
@@ -1,23 +1,70 @@
+import java.util.ArrayList;
+
 class Ex01 {
-  public static void main(String[] agrs) {
-    // O atributo estatico pode ser alterado via:
+  public static void main(String[] args) {
+    ArrayList<Livros> livros = new ArrayList<Livros>();
 
-    // Object
-    new Livro("livro 1", "Saulo", "Ciências Humanas", "hello world", "1.2");
+    livros.add( new Livros("Livro 1", "Saulo", Livros.tipoGenero[0], "Saulos editora", "1ª") );
+    livros.add( new Livros("Livro 2", "Saulo 2", Livros.tipoGenero[1], "Saulos editora", "edição 2") );
+    livros.add( new Livros("Livro 3", "Saulo 3", Livros.tipoGenero[2], "Saulos editora", "edição 3") );
 
-    // E diretamente pela classe
-    Livro.autor = "Teste";
+    for (Livros livro: livros) {
+      System.out.println(livro.toString());
+    }
+    
   }
 }
 
-class Livro {
-  public static String titulo, autor, genero, editora, edicao;
+class Livros {
+  private String titulo, autor, genero, editora, edicao;
+  static String[] tipoGenero = {"ficção", "ciências naturais", "literatura", "ciências humanas"};
 
-  Livro(String titulo, String autor, String genero, String editora, String edicao) {
-    Livro.titulo = titulo;
-    Livro.autor = autor; 
-    Livro.genero = genero; 
-    Livro.editora = editora; 
-    Livro.edicao = edicao;
+  Livros(String titulo, String autor, String genero, String editora, String edicao) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.genero = genero;
+    this.editora = editora;
+    this.edicao = edicao;
+  }
+
+  String getTitulo() {
+    return this.titulo;
+  } 
+  String getAutor() {
+    return this.autor;
+  } 
+  String getGenero() {
+    return this.genero;
+  } 
+  
+  String getEditora() {
+    return this.editora;
+  } 
+
+  String getEdicao() {
+    return this.edicao;
+  }
+
+  void setTitulo(String titulo) {
+    this.titulo = titulo;
+  } 
+  void setAutor(String autor) {
+    this.autor = autor;
+  } 
+  void setGenero(String genero) {
+    this.genero = genero;
+  } 
+  
+  void setEditora(String editora) {
+    this.editora = editora;
+  } 
+
+  void setEdicao(String edicao) {
+    this.edicao = edicao;
+  }
+
+  @Override
+  public String toString() {
+    return this.titulo + ", " + this.autor + ", " + this.genero + ", " + this.editora + ", " + this.edicao;
   }
 }
--- List08/Ex02.java
+++ List08/Ex02.java
@@ -1,55 +1,60 @@
-public class Ex02 {
+class Ex02 {
   public static void main(String[] args) {
-    Quadrado quadrado = new Quadrado();
-    Retangulo retangulo = new Retangulo();
+    Quadrado quadrado = new Quadrado(4, 5, 5);
+    Trapezio trapezio = new Trapezio(4, 5, 4);
+    Triangulo triangulo = new Triangulo(3, 6, 2);
 
     quadrado.desenha();
-    retangulo.desenha();
-  }  
+    trapezio.desenha();
+    triangulo.desenha();
+  }
 }
 
 abstract class FiguraAbstrata {
-  int altura, largura;
+  public int quantLados;
+  public double altura, largura;
 
-  public void desenhLinha() {
-    System.out.print(" ");
-    for (int j = 0; j < largura; j++)
-      System.out.print("-");
+  protected abstract void desenha();
+
+  FiguraAbstrata(int quantLados, double altura, double largura) {
+    this.quantLados = quantLados;
+    this.altura = altura;
+    this.largura = largura;
+  };
+}
+
+class Trapezio extends FiguraAbstrata {
+  
+  Trapezio(int quantLados, double altura, double largura) {
+    super(quantLados, altura, largura);
   }
-  
-  public void desenha() {
-    desenhLinha();
 
-    System.out.println();
-
-    for (int k = 0; k < altura; k++) {
-      System.out.print("|");
-      
-      for (int j = 0; j < largura; j++) {
-        System.out.print(" ");
-      }
-
-      System.out.print("|");
-      System.out.println();
-    }
-  
-    desenhLinha();
-    System.out.println();
+  @Override
+  protected void desenha() {
+    System.out.println("Desenhando trapezio...");    
   }
 }
 
 class Quadrado extends FiguraAbstrata {
   
-  Quadrado() {
-    this.altura = 2;
-    this.largura = 4;
+  Quadrado(int quantLados, double altura, double largura) {
+    super(quantLados, altura, largura);
   }
 
+  @Override
+  protected void desenha() {
+    System.out.println("Desenhando Quadrado...");    
+  }
 }
 
-class Retangulo extends FiguraAbstrata {
-  Retangulo() {
-    this.altura = 4;
-    this.largura = 16;
+class Triangulo extends FiguraAbstrata {
+  
+  Triangulo(int quantLados, double altura, double largura) {
+    super(quantLados, altura, largura);
+  }
+
+  @Override
+  protected void desenha() {
+    System.out.println("Desenhando Triangulo...");    
   }
 }
